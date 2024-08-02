1b09857015170f329c051a7ba2cb9c4694b177de
GabrielReyes <gabrielreyes2318@gmail.com>
2024-08-02 18:22:21 -0400

    open and close modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,50 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
     ),
   },
   {
     name: "Lake Louise",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
     ),
   },
   {
     name: "Bald Mountains",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
     ),
   },
   {
     name: "Latemar",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
     ),
   },
   {
     name: "Vanoise National Park",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
     ),
   },
   {
     name: "Lago di Braies",
-    link: URL(
+    link: new URL(
       "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
     ),
   },
 ];
+
+const profileEditButton = document.querySelector(".js-profile-edit-button");
+const modalCloseButton = document.querySelector(".js-modal-close-button");
+const modal = document.querySelector(".js-modal");
+
+profileEditButton.addEventListener("click", () => {
+  return modal.classList.add("modal__opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  return modal.classList.remove("modal__opened");
+});
