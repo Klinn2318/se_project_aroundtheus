0454fe91822e26daff2469cb84a50d7b8f9068ba
GabrielReyes <gabrielreyes2318@gmail.com>
2024-08-24 04:15:48 -0400

    feat/ 4 to 7 steps in tripleten 5th project
    *add delete button and functionality
    *add functionality to like button
    *add image modals
    *add smooth transitions for modals

--- scripts/index.js
+++ scripts/index.js
@@ -36,8 +36,8 @@ const modalForm = modal.querySelector("#js-modal-form");
 const inputTitle = modal.querySelector("#input-title");
 const inputDescription = modal.querySelector("#input-description");
 const galleryList = document.querySelector("#js-gallery-list");
-const templateGallery =
-  document.querySelector("#js-card-template").content.firstElementChild;
+const templateGallery = document.querySelector("#js-card-template").content;
+const templateImage = document.querySelector("#image-modal").content;
 
 function closePopUp() {
   modal.classList.remove("modal_opened");
@@ -58,11 +58,49 @@ modalForm.addEventListener("submit", (event) => {
   closePopUp();
 });
 
+function toggleClass(element, elementClass) {
+  return element.classList.toggle(elementClass);
+}
+
+function deleteElement(element, elementClass) {
+  return element.closest(elementClass).remove();
+}
+
+function getImageModal(imageData) {
+  const imageElement = templateImage.cloneNode(true);
+  const imageModal = imageElement.querySelector(".image__modal");
+  const imagePic = imageElement.querySelector(".image__picture");
+  const imageName = imageElement.querySelector(".image__name");
+  const imageBtn = imageElement.querySelector(".image__close-btn");
+
+  imagePic.src = imageData.link;
+  imagePic.alt = imageData.name;
+  imageName.textContent = imageData.name;
+  imageBtn.addEventListener("click", () => {
+    toggleClass(imageModal, "image__opened");
+    setTimeout(() => deleteElement(imageBtn, ".image__modal"), 500);
+  });
+
+  setTimeout(() => {
+    toggleClass(imageModal, "image__opened");
+  }, 10);
+  return modal.before(imageElement);
+}
+
 function getCardElement(cardData) {
   const cardElement = templateGallery.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+  const cardLike = cardElement.querySelector(".card__like");
+  const cardDeleteBtn = cardElement.querySelector(".card__delete-button");
 
+  cardLike.addEventListener("click", () =>
+    toggleClass(cardLike, "card__like_actived")
+  );
+  cardDeleteBtn.addEventListener("click", () =>
+    deleteElement(cardDeleteBtn, ".card")
+  );
+  cardImage.addEventListener("click", () => getImageModal(cardData));
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
@@ -72,5 +110,5 @@ function getCardElement(cardData) {
 
 initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
-  galleryList.prepend(cardElement);
+  galleryList.append(cardElement);
 });
